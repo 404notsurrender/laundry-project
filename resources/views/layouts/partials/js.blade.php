ab271a12e6ec5b100d0e9587fda295e11ce68d90
Florentyna Senjoyo <florentynasenjoyo@Florentynas-MacBook-Pro.local>
2023-05-25 13:39:20 +0700

    initial commit

---
+++ resources/views/layouts/partials/js.blade.php
@@ -0,0 +1,11 @@
+@section('js')
+<script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-ui.js') }}"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/js/aos.js') }}"></script>
+
+<script src="{{ asset('assets/js/main.js') }}"></script>
+@endsection
